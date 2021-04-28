95413b7223b7eeb7a31f81817d83889d7a83a19b
Nathan Waterman <nathanjwtx@nathanjwtx.io>
2021-04-28 07:31:40 -0500

    section 3 - setup basic pages and controller

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return Index();
+        }
+
+        public IActionResult Error()
+        {
+            return Error();
+        }
+    }
+}
---
+++ WishList/Views/Shared/Error.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WishList.Views.Shared
+{
+    public class Error : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
